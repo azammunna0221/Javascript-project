a48c6f752692155b17e368f8cc27d00284199141
Ali Azam <azammunna0221>
2023-02-18 22:12:34 +0600

    traiangle function created

--- js/calcucaltion.js
+++ js/calcucaltion.js
@@ -1,7 +1,17 @@
-// function for triangle 
+// function for getting input from input field.
 function getInputFieldById(input){
-    const inputValue = document.getElementById('triangle-b');
+    const inputValue = document.getElementById(input);
     const inputValueString = inputValue.value;
     const inputValueNumber = parseFloat(inputValueString);
+    //inputValue.value = '';
     return inputValueNumber;
 }
+
+// function for Triangle Area. 
+document.getElementById('triangle-btn').addEventListener('click',function(){
+    const triangleBase = getInputFieldById('triangle-b');
+    const triangleHeight = getInputFieldById('triangle-h');
+
+    const triangleArea = 0.5 * triangleBase * triangleHeight;
+    console.log(triangleArea);
+})
