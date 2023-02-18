b14888f42bb508dd5ab790c71e4fe7577821a00b
Ali Azam <azammunna0221>
2023-02-18 23:25:49 +0600

    display function complete

--- js/calcucaltion.js
+++ js/calcucaltion.js
@@ -6,6 +6,12 @@ function getInputFieldById(input){
     //inputValue.value = '';
     return inputValueNumber;
 }
+// Function for Set Element in the display the display
+/* function setElementById(elementId, newValue){
+    const newScreenValue = document.getElementById(elementId);
+    newScreenValue.innerText = newValue;
+} */
+
 //Area [multiplication of Two Number]
 
 // function for Triangle Area. 
@@ -14,7 +20,11 @@ document.getElementById('triangle-btn').addEventListener('click',function(){
     const triangleHeight = getInputFieldById('triangle-h');
 
     const triangleArea = 0.5 * triangleBase * triangleHeight;
-    console.log(triangleArea);
+
+    const displayValue = document.getElementById('list-item1');
+    displayValue.innerText = triangleArea;
+
+    //console.log(triangleArea);
 })
 
 //function for Rectangle Area.
@@ -23,7 +33,9 @@ document.getElementById('rectangle-btn').addEventListener('click', function(){
     const rectangleLength = getInputFieldById('rectangle-l');
 
     const rectangleArea = rectangleLength * rectangleWidth;
-    console.log(rectangleArea);
+    
+    const displayValue = document.getElementById('list-item2');
+    displayValue.innerText = triangleArea;
 })
 
 //function for parallelogram Area.
@@ -32,5 +44,40 @@ document.getElementById('parallelogram-btn').addEventListener('click', function(
     const parallelogramHight = getInputFieldById('parallelogram-h');
 
     const parallelogramArea = parallelogramBase * parallelogramHight;
-    console.log(parallelogramArea);
+    
+    const displayValue = document.getElementById('list-item3');
+    displayValue.innerText = triangleArea;
+})
+
+//function for rhombus Area.
+document.getElementById('rhombus-btn').addEventListener('click', function(){
+    const rhombusd1 = getInputFieldById('rhombus-d1');
+    const rhombusd2 = getInputFieldById('rhombus-d2');
+
+    const rhombusArea = 0.5 * rhombusd1 * rhombusd2;
+
+    const displayValue = document.getElementById('list-item4');
+    displayValue.innerText = rhombusArea;
+})
+
+//function for pentagon Area.
+document.getElementById('pentagon-btn').addEventListener('click', function(){
+    const pentagonParameter = getInputFieldById('pentagon-p');
+    const pentagonBase = getInputFieldById('pentagon-b');
+
+    const pentagonArea = 0.5 * pentagonParameter * pentagonBase;
+
+    const displayValue = document.getElementById('list-item5');
+    displayValue.innerText = pentagonArea;
+})
+
+//function for ellipse Area.
+document.getElementById('rhombus-btn').addEventListener('click', function(){
+    const ellipseA = getInputFieldById('ellipse-a');
+    const ellipseB = getInputFieldById('ellipse-b');
+
+    const ellipseArea = 3.1416 * ellipseA * ellipseB;
+
+    const displayValue = document.getElementById('list-item6');
+    displayValue.innerText = ellipseArea;
 })
